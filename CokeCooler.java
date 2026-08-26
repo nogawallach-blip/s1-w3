96bd23b52c531ab30a82c80201981c0bb3354b57
Noga Wallach <noga.wallach@gmail.com>
2026-08-26 12:25:52 -0700

    cokecooler done!

--- CokeCooler.java
+++ CokeCooler.java
@@ -1,3 +1,23 @@
 public class CokeCooler {
-    
+    private int numCokes;
+
+    public CokeCooler(int n){
+        numCokes = n; 
+    }
+    public CokeCooler(){
+        numCokes = 100;
+    }
+
+
+    public void remove(int r){
+        numCokes -= r;
+    }
+
+    public void fill(int f){
+        numCokes += f;
+    }
+
+    public void printState(){
+        System.out.println("Number of Cokes: " + numCokes);
+    }
 }
---
+++ CokeCoolerMain.java
@@ -0,0 +1,15 @@
+public class CokeCoolerMain{
+    public static void main(String[] args){
+
+        CokeCooler c1 = new CokeCooler();
+
+        c1.remove(20);
+        c1.printState();
+        c1.fill(5);
+        c1.printState();
+
+    }
+
+
+
+}
