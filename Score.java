43aef20a2919b9009752c016bbac87aab5af89fd
Noga Wallach <noga.wallach@gmail.com>
2026-08-28 11:47:53 -0700

    done

--- Score.java
+++ Score.java
@@ -1,3 +1,29 @@
 public class Score {
+    private int points;
+
+    public Score(){
+        points = 50;
+
+    }
+
+    public void score(int p){
+        points = p;
+    }
+
+    public void dec(int d){
+        points -= d;
     
+    }
+
+    public void increase(int n){
+        points += n;
+
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
+
+
 }
---
+++ ScoreMain.java
@@ -0,0 +1,11 @@
+public class ScoreMain {
+    public static void main(String[] args) {
+
+        Score s1 = new Score();
+
+        s1.increase(40);
+
+      System.out.println("Total score: " + s1.getPoints());
+
+    }
+}
